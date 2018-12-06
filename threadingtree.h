236824bedd3a38bf7d209f9103a38577dba6e1a8
honk2333 <34983184+honk2333@users.noreply.github.com>
2018-12-07 00:06:57 +0800

    Add files via upload

--- threadingtree.h
+++ threadingtree.h
@@ -2,27 +2,32 @@
 #include<cstdio>
 #include<cstring>
 #include<cstdlib>
-using namespace std;
+
 
 typedef int Type;
-typedef struct BitNode
-{
-       Type data; //结点数据
-       struct BitNode *lchild, *rchild, *prior, *next; //左右孩子指针以及前驱后继指针
-}BitNode, *Bitree;
+typedef struct BitNode {
+    Type data; //结点数据
+    struct BitNode *lchild, *rchild, *prior, *next; //左右孩子指针以及前驱后继指针
+} BitNode, *Bitree;
 
-int Initree(Bitree& T);
+int Initree(Bitree& T, Type e);
+int BulidTree(Bitree& T);
 int Destory(Bitree& T);
-int Search(Bitree T,Type x,Bitree& Childtree);
-int Deepth(Bitree T,Type x);
+int Search(Bitree T, Type x, Bitree& Childtree);
+int Deepth(Bitree T, Type x);
 int TreeDepth(Bitree T);
-Type Insert(Type e,Bitree& T);
-Type Delete_x(Type x,Bitree& T);
-Type DeleteNode(Bitree& T,Type& e,BitNode* p);
-Bitree CreatPirorThread(Bitree &T);
+int Insert(Type e, Bitree& T);
+int Delete(Bitree T, Bitree& Thrt);
+int Delete_x(Type x, Bitree T);
+int DeleteNode(Bitree T, Bitree& p);
+int CreatpriorThread(Bitree &T);
 Bitree CreatMiddleThread(Bitree &T);
 Bitree CreatPostThread(Bitree &T);
 int RemoveThread(Bitree& T);
 void Travel_prior(Bitree T);
 void Travel_middle(Bitree T);
 void Travel_post(Bitree T);
+int FindPriorAndNext(Bitree T, Type &priorelem, Type &nextelem);
+int FindParent(Bitree T, Bitree& parent, BitNode *s);
+int FindParentOfX(Bitree T, Type x, Bitree& parent);
+int FindParentOfX(Bitree T, Bitree& parent);
